4bb892bb381f214c712cc47f293305bbc520ef6f
BhavanaShrotri <bhavanasm18@gmail.com>
2021-04-17 21:52:10 +0530

    Added composition to code

--- Object-Oriented Programming Fundamentals in C#/CustomerManagement/CustomerManagement/CustomerRepository.cs
+++ Object-Oriented Programming Fundamentals in C#/CustomerManagement/CustomerManagement/CustomerRepository.cs
@@ -8,6 +8,11 @@ namespace CustomerManagement
 {
     public class CustomerRepository
     {
+        public CustomerRepository()
+        {
+            addressRepository = new AddressRepository();
+        }
+        private AddressRepository addressRepository { get; set; }
         public Customer Retrieve(int custId)
         {
             Customer customer = new Customer(custId);
@@ -17,6 +22,7 @@ namespace CustomerManagement
                 customer.EmailAddress = "bhavana@gmail.com";
                 customer.FirstName = "Bhavana";
                 customer.LastName = "Shrotri";
+                customer.AdressList = addressRepository.RetriveByCustomerId(custId).ToList();
             }
 
             return customer;
