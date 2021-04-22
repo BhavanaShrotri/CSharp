2301a984eb5e5ceb52d393c3291d96d215aefd1a
BhavanaShrotri <bhavanasm18@gmail.com>
2021-04-22 22:39:08 +0530

    Added try and catch block to handle exception

---
+++ Error Handling in C# with Exceptions/ConsoleCalculator/ConsoleCalculator/Calculator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleCalculator
+{
+    class Calculator
+    {
+        public int Calculate(int number1, int number2, string operation)
+        { 
+            if(operation == "/")
+            {
+                return Divide(number1, number2); 
+            }
+            else
+            {
+                throw new ArgumentOutOfRangeException(nameof(operation),
+                    "Mathematical operator is not supported.");
+                //Console.WriteLine("Unkown Operation");
+                //return 0;
+            }
+        
+        }
+
+        private int Divide(int number, int divisor)
+        {
+            return number / divisor;
+        }
+    }
+}
---
+++ Error Handling in C# with Exceptions/ConsoleCalculator/ConsoleCalculator/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+
+namespace ConsoleCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Enter First Number : ");
+            int number1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter Second Number : ");
+            int number2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter Operation: ");
+            string operation = Console.ReadLine().ToUpperInvariant();
+
+            var calculator = new Calculator();
+
+            try
+            {
+                int result = calculator.Calculate(number1, number2, operation);
+                DisplayResult(result);
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine($"Sorry Somthing Went Wrong. {ex}");
+            } 
+
+
+            
+
+        }
+
+        private static void DisplayResult(int result)
+        {
+            Console.WriteLine($"Result is :{result}");
+        }
+    }
+}
